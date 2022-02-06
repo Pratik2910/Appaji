5d04c9f06f11f237b6835995867c459bc99f21e7
Pratik2910 <74420211+Pratik2910@users.noreply.github.com>
2022-02-06 11:58:57 +0530

    Update Form-Handler.php

--- Form-Handler.php
+++ Form-Handler.php
@@ -5,24 +5,17 @@ $subject=$_POST['subject'];
 $message=$_POST['message'];
 
 $email_form = 'pratikrajurkar34@gmail.com';
-$email_subject ='New From Submission';
-$email_body ="User Name: $name.\n".
+$email_subject = "New From Submission";
+$email_body = "User Name: $name.\n".
                 "User Email: $visitor_email.\n".
-                "subject: $subject.\n".
-                "User Message: $message.\n";
+                 "subject: $subject.\n".
+                  "User Message: $message.\n";
 $to = 'pratikrajurkar34@gmail.com';
 $headers = "Form: $email_form \r\n";
 $headers = "Reply-To: $visitor_email \r\n";
 
 
-mail($to $email_subject,$email_body,$headers);
+mail($to.$email_subject,$email_body,$headers);
 header("Location: Contacts.html")
-
-
-
-
-
-
-
 
 ?>
